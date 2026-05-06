c49a21c3a928d363e8c37f92dec0fe1cc0d17f01
zyavuz610 <43997998+zyavuz610@users.noreply.github.com>
2026-05-06 23:17:30 +0300

    .

--- paralel/trapez_io2.c
+++ paralel/trapez_io2.c
@@ -3,18 +3,15 @@
 mpicc trapez.c -o trapez
 mpirun -np 6 ./trapez
 
-
 */
 
 #include <stdio.h>
 #include <mpi.h>
 
-// İntegrali alınacak fonksiyon (Örn: f(x) = x*x)
 double f(double x) {
     return x * x;
 }
 
-// Yamuk Kuralı hesaplama fonksiyonu
 double Trap(double left_end, double right_end, int trap_count, double base_len) {
     double estimate, x;
     int i;
@@ -29,7 +26,7 @@ double Trap(double left_end, double right_end, int trap_count, double base_len) 
 }
 
 int main(int argc, char** argv) {
-    int my_rank, comm_sz, n, local_n;   
+    int my_rank, comm_sz, n, local_n;
     double a, b, h, local_a, local_b;
     double local_int, total_int;
 
@@ -37,55 +34,38 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-    // rank=0 olan process'te değerleri kullanıcıdan alalım
     if (my_rank == 0) {
         printf("Integralin başlangıç noktası (a) giriniz: ");
+        fflush(stdout);
         scanf("%lf", &a);
+
         printf("Integralin bitiş noktası (b) giriniz: ");
+        fflush(stdout);
         scanf("%lf", &b);
+
         printf("Yamuk sayısı (n) giriniz: ");
+        fflush(stdout);
         scanf("%d", &n);
     }
 
-    // rank=0 olan process'ten for döngüsü ile diğer processlere değerleri gönder
-    if (my_rank == 0) {
-        for (int i = 1; i < comm_sz; i++) {
-            MPI_Send(&a, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&b, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
-            MPI_Send(&n, 1, MPI_INT, i, 2, MPI_COMM_WORLD);
-        }
-    } else {
-        // rank=0 olmayan processlerde MPI_Recv ile değerleri al
-        MPI_Recv(&a, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&b, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(&n, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
+    MPI_Bcast(&a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&b, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&n, 1, MPI_INT,    0, MPI_COMM_WORLD);
 
-    h = (b - a) / n;          // Her bir yamuğun genişliği
-    local_n = n / comm_sz;    // Her işlemciye düşen yamuk sayısı
+    h       = (b - a) / n;
+    local_n = n / comm_sz;
 
-    // Her işlemcinin kendi hesaplama aralığını belirle
     local_a = a + my_rank * local_n * h;
     local_b = local_a + local_n * h;
-    
-    // Kendi payına düşen alanı hesapla
+
     local_int = Trap(local_a, local_b, local_n, h);
 
-    // Tüm yerel sonuçları 0 numaralı (Master) işlemcide topla
-    if (my_rank != 0) {
-        printf("%d/%d biriminde (%f - %f) arası integral hesaplanıyor\n", my_rank, comm_sz, local_a, local_b);
-        MPI_Send(&local_int, 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
-    } else {
-        total_int = local_int;
-        for (int i = 1; i < comm_sz; i++) {
-            MPI_Recv(&local_int, 1, MPI_DOUBLE, i, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            total_int += local_int;
-        }
-    }
+    printf("Rank %d/%d → hesaplanan aralık: [%f, %f]\n", my_rank, comm_sz, local_a, local_b);
 
-    // Sonucu sadece Master işlemci yazdırsın
+    MPI_Reduce(&local_int, &total_int, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
     if (my_rank == 0) {
-        printf("%d adet yamuk ile integral tahmini (%.1f'den %.1f'e) = %.15e\n", n, a, b, total_int);
+        printf("\n%d adet yamuk ile integral tahmini (%.1f'den %.1f'e) = %.15e\n", n, a, b, total_int);
     }
 
     MPI_Finalize();
